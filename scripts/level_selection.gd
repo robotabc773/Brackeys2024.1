5bf084f949dc699de6a935873073b8ff4976eaed
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2024-02-13 22:33:22 -0500

    Added level selection scene

---
+++ scripts/level_selection.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+
+func _on_level_1_pressed():
+	# get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	pass # Replace with function body.
+	
+
+
+
+func _on_level_2_pressed():
+	# get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	pass # Replace with function body.
