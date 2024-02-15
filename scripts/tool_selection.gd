5663e7a55f562a342a771cad41cc3bbac8aae028
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2024-02-15 00:30:52 -0500

    Updated tool selection UI, not working yet

--- scripts/tool_selection.gd
+++ scripts/tool_selection.gd
@@ -1,16 +1,14 @@
 extends Control
 
-
-
-
+var logic_grid = preload("res://scripts/logic_grid.gd").new()
 
 func _on_tool_1_pressed():
-	pass # Replace with function body.
+	logic_grid.switch_tools("res://scripts/tools/tool_a.gd")
 
 
 
 func _on_tool_2_pressed():
-	pass # Replace with function body.
+	logic_grid.switch_tools("res://scripts/tools/tool_b.gd")
 
 
 
