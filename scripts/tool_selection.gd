fad4ddd8e1eaace111b3e650468b569b7e20aaa0
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2024-02-14 23:52:11 -0500

    Added tool selection UI without functionality

---
+++ scripts/tool_selection.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+
+
+func _on_tool_1_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_tool_2_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_tool_3_pressed():
+	pass # Replace with function body.
