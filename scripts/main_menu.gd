a18ee78980c5dd19cc318a0609fe927a5fe941c0
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2024-02-18 02:48:45 -0500

    Added sfx for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,22 +1,23 @@
 extends Control
+var sfxPlayer := AudioStreamPlayer.new()
 
-
-
-
+func _ready():
+	add_child(sfxPlayer)
+	var sfx = load("res://sound/sfx/UI_pop.wav")
+	sfxPlayer.stream = sfx
 
 
 func _on_play_pressed() -> void:
+	sfxPlayer.play()
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 
 
-
-
 func _on_exit_pressed() -> void:
+	sfxPlayer.play()
 	get_tree().quit()
 
 
-
-
 func _on_credits_pressed() -> void:
+	sfxPlayer.play()
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
