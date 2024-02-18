b76b2968f49087d5477ecbd6869b1a4860aaffe2
robotabc773 <sebastianxwu@gmail.com>
2024-02-18 00:48:52 -0500

    tool selection

--- scripts/level_loader.gd
+++ scripts/level_loader.gd
@@ -7,22 +7,21 @@ var stamp_T : Tool = tool_stamp.new([Vector2i(0, 0), Vector2i(-1, 0), Vector2i(1
 var stamp_J : Tool = tool_stamp.new([Vector2i(0, 0), Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 1)])
 	
 var levels : Array[Level] = [
-	#Level.new([], Grid.from_json('')),
+	#Level.new("", [], Grid.from_json('')),
 	Level.new("Left-click to stamp", [stamp_dot], Grid.from_json('{"num_cols":3,"num_rows":3,"states":[2,2,2,2,0,2,2,2,2]}')),
 	Level.new("Fill it all in!", [stamp_dot], Grid.from_json('{"num_cols":4,"num_rows":3,"states":[2,2,2,2,2,0,0,2,2,2,2,2]}')),
 	Level.new("Arrow keys to rotate", [stamp_pair], Grid.from_json('{"num_cols":9,"num_rows":4,"states":[2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2]}')),
 	Level.new("Right-click to undo, R to restart", [stamp_pair], Grid.from_json('{"num_cols":8,"num_rows":6,"states":[2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,2,2,2,0,0,2,0,2,2,2,0,2,0,0,0,0,2,2,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2]}')),
 	Level.new("Stamping flips tiles", [stamp_pair], Grid.from_json('{"num_cols":6,"num_rows":6,"states":[2,2,2,2,2,2,2,0,0,0,0,2,2,2,0,2,0,2,2,0,0,0,0,2,2,2,0,2,0,2,2,2,2,2,2,2]}')),
-	Level.new("", [stamp_T, stamp_J], Grid.from_json('{"num_cols":4,"num_rows":4,"states":[0,0,0,0,2,0,2,0,2,0,0,0,0,0,0,2]}')),
+	Level.new("Multiple stamps!", [stamp_T, stamp_J], Grid.from_json('{"num_cols":4,"num_rows":4,"states":[0,0,0,0,2,0,2,0,2,0,0,0,0,0,0,2]}')),
 ]
 
 var current_level : int
 
-@onready var logic_grid : LogicGrid = $LogicGridContainer/LogicGrid
-@onready var logic_grid_container : AspectRatioContainer = $LogicGridContainer
-# TODO: tool selection existing
-#@onready var tool_selection : ToolSelection = $ToolSelection
-@onready var level_title : Label = $LevelTitle
+@onready var logic_grid : LogicGrid = %LogicGrid
+@onready var logic_grid_container : AspectRatioContainer = %"LogicGrid/.."
+@onready var stamp_select : StampSelect = %StampSelect
+@onready var level_title : Label = %LevelTitle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -51,8 +50,7 @@ func load_level(id : int) -> void:
 	current_level = id
 	var level := levels[current_level]
 	logic_grid.set_grid(level.initial_grid)
-	#tool_selection.set_tools(level.possible_tools)
-	# TODO: this is temporary
+	stamp_select.set_tools(level.possible_tools)
 	logic_grid.current_tool = level.possible_tools[0]
 	
 	logic_grid_container.ratio = float(level.initial_grid.num_cols) / (level.initial_grid.num_rows)
--- scripts/tools/tool.gd
+++ scripts/tools/tool.gd
@@ -2,8 +2,11 @@ class_name Tool
 extends RefCounted
 ## Base class for a tool, shouldn't be used directly
 
+
 ## Whether this tool just takes one click instead of click and release
 var one_click := false
+## The image to use for this tool in the selection menu
+var icon : Texture2D = preload("res://sprites/tile_dark.tres")
 
 
 ## Should return true iff the given position is valid to start the tool at
