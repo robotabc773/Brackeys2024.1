164e2ac2849b0d087895a3dee353de6bbd072f77
robotabc773 <sebastianxwu@gmail.com>
2024-02-18 01:46:41 -0500

    type fix

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,18 +5,18 @@ extends Control
 
 
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 
 
 
 
-func _on_exit_pressed():
+func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
 
 
-func _on_credits_pressed():
+func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
